652ed55be1ac557895f37e54742e104d03187724
vitalyan <avit2001@gmail.com>
2020-06-20 19:39:35 +0300

    change port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const host = "5.9.55.116";
-const port = "5000";
+const port = process.env.PORT || 5000;
 const mysql = require("mysql");
 
 app.get("/message", function(request, response) {
